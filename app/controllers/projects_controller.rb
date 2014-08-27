985e4bda4a859c2b5915897dc8210cbfe67237d2
Eric <erjohnson.pdx@gmail.com>
2014-08-27 03:41:12 +0000

    Add projects#create handler

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,6 +9,15 @@ class ProjectsController < ApplicationController
     render('projects/new.html.erb')
   end
   
+  def create
+    @project = Project.new(params[:project])
+    if @project.save
+      redirect_to("/projects/#{@project.id}")
+    else
+      render('projects/new.html.erb')
+    end
+  end
+  
   def show
     @project = Project.find(params[:id])
     render('projects/show.html.erb')
