74cfaec3faff56f94249dacb28209545b5442955
Eric <erjohnson.pdx@gmail.com>
2014-08-27 09:52:35 -0700

    Add stories#create handler

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -4,4 +4,13 @@ class StoriesController < ApplicationController
     @story = project.stories.new
     render('stories/new.html.erb')
   end
+
+  def create
+    @story = project.stories.new(params[:story])
+    if @story.save
+      redirect_to("/projects/#{@story.project_id}")
+    else
+      render('stories/new.html.erb')
+    end
+  end
 end
