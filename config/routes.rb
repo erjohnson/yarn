c98328e6253ce85fbdeaee5a17a9b642016cc3ea
Eric <erjohnson.pdx@gmail.com>
2014-08-27 03:57:29 +0000

    Add projects#edit route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   match('projects/new', {:via => :get, :to => 'projects#new'})
   match('projects', {:via => :post, :to => 'projects#create'})
   match('projects/:id', {:via => :get, :to => 'projects#show'})
+  match('projects/:id/edit', {:via => :get, :to => 'projects#edit'})
 end
