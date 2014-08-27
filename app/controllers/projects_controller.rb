d5ee61bb6468a4177a8fe59acd6f96bb88ca66b6
Eric <erjohnson.pdx@gmail.com>
2014-08-27 03:22:55 +0000

    Add projects#show handler

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,4 +8,9 @@ class ProjectsController < ApplicationController
     @project = Project.new
     render('projects/new.html.erb')
   end
+  
+  def show
+    @project = Porject.find(params[:id])
+    render('projects/show.html.erb')
+  end
 end
