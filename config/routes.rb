693b0794e8677cf3efc2c71d0caa07c2c7b2432c
Eric <erjohnson.pdx@gmail.com>
2014-08-27 03:22:21 +0000

    Add projects#show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   match('projects', {:via => :get, :to => 'projects#index'})
   match('projects/new', {:via => :get, :to => 'projects#new'})
   match('projects', {:via => :post, :to => 'projects#create'})
+  match('projects/:id', {:via => :get, :to => 'projects#show'})
 end
