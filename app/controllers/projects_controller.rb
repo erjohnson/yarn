3fedd4164d855d0bcecac2e5bb173d40219b22cb
Eric <erjohnson.pdx@gmail.com>
2014-08-27 11:44:48 -0700

    Add flash message to projects#create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,31 +3,32 @@ class ProjectsController < ApplicationController
     @projects = Project.all
     render('projects/index.html.erb')
   end
-  
+
   def new
     @project = Project.new
     render('projects/new.html.erb')
   end
-  
+
   def create
     @project = Project.new(params[:project])
     if @project.save
+      flash[:notice] = "Your project was added to Yarn."
       redirect_to("/projects/#{@project.id}")
     else
       render('projects/new.html.erb')
     end
   end
-  
+
   def show
     @project = Project.find(params[:id])
     render('projects/show.html.erb')
   end
-  
+
   def edit
     @project = Project.find(params[:id])
     render('projects/edit.html.erb')
   end
-  
+
   def update
     @project = Project.find(params[:id])
     if @project.update(params[:project])
@@ -36,7 +37,7 @@ class ProjectsController < ApplicationController
       render('projects/edit.html.erb')
     end
   end
-  
+
   def destroy
     @project = Project.find(params[:id])
     @project.destroy
